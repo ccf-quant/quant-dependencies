921974572ebe30adcab61a0bb026262fba40516a
Haowen Xu <haowen.xu@outlook.com>
2020-12-15 16:50:13 +0800

    updated

--- native/quant-backtest/install.sh
+++ native/quant-backtest/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp "${SOURCE_DIR}/dist/qe-backtest" /usr/local/bin/qe-backteset
-cp "${SOURCE_DIR}/dist/qe-backtest-cache.py" /usr/local/bin/qe-backteset-cache.py
+cp "${SOURCE_DIR}/dist/qe-backtest" /usr/local/bin/qe-backtest
+cp "${SOURCE_DIR}/dist/qe-backtest-cache.py" /usr/local/bin/qe-backtest-cache.py
 chmod +x /usr/local/bin/qe-backtest
 chmod +x /usr/local/bin/qe-backtest-cache.py
